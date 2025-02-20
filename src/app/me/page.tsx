00968518dd5b31226ae853fae44c6745a4b39db2
HuuLoc <huuloc@192.168.1.16.non-exists.ptr.local>
2025-02-20 19:06:09 +0700

    init project nextjs tailwind shadcnui

---
+++ src/app/me/page.tsx
@@ -0,0 +1,3 @@
+export default function MePage() {
+  return <h1>Me Page</h1>;
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <Button asChild>
+        <Link href="/login">Login</Link>
+      </Button>
+
+      <Button asChild>
+        <Link href="/register">Register</Link>
+      </Button>
+
+      <Button asChild>
+        <Link href="/me">me</Link>
+      </Button>
+    </>
+  );
+}
